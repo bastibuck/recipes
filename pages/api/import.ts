277c5de5ef822478907d38741787e9e827cf0956
Basti Buck <mail@bastibuck.de>
2021-10-08 19:34:06 +0200

    prettify code

--- pages/api/import.ts
+++ pages/api/import.ts
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  import: string
-}
+  import: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ import: 'okay' })
+  res.status(200).json({ import: "okay" });
 }
